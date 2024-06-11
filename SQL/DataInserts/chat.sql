bc5cf4282564a1b9b9b922516d8041682875c006
JMLTUnderCode <juniormiguel08062@gmail.com>
2024-06-10 22:05:40 -0400

    Carpeta de ordenamiento para inserts de prueba en la base de datos.

---
+++ SQL/DataInserts/chat.sql
@@ -0,0 +1,5 @@
+insert into chat (id_chat) values (1);
+insert into chat (id_chat) values (2);
+insert into chat (id_chat) values (3);
+insert into chat (id_chat) values (4);
+insert into chat (id_chat) values (5);
