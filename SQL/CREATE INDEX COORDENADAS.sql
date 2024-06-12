620892e8c9f43c9979becb3bcd06e09abc00a668
Ana Shek <73360276+anna-ayn@users.noreply.github.com>
2024-06-11 21:08:33 -0400

    Update CREATE INDEX COORDENADAS.sql

--- SQL/CREATE INDEX COORDENADAS.sql
+++ SQL/CREATE INDEX COORDENADAS.sql
@@ -1,5 +1,5 @@
-CREATE INDEX perfil_geo_index ON perfil USING GIST (ST_SetSRID(ST_MakePoint(longitud, latitud), 4326));
+CREATE INDEX perfil_geo_index ON perfil USING GIST (coordenada);
 
-CREATE INDEX pref_geo_index ON preferencias USING GIST (ST_SetSRID(ST_MakePoint(latitud_origen, longitud_origen), 4326));
+CREATE INDEX pref_geo_index ON preferencias USING GIST (coordenada_origen);
 
-CREATE INDEX institucion_geo_index ON institucion USING GIST (ST_SetSRID(ST_MakePoint(latitud, longitud), 4326));
+CREATE INDEX institucion_geo_index ON institucion USING GIST (coordenada);
