fa08e2037bc8211d014c660e20c8b2ea009ea10e
Ana Shek <73360276+anna-ayn@users.noreply.github.com>
2024-06-11 22:49:58 -0400

    Update triggers.sql

--- SQL/triggers.sql
+++ SQL/triggers.sql
@@ -1,14 +1,14 @@
 CREATE TRIGGER institucion_insert_coordenada_trigger
-BEFORE INSERT ON institucion
+BEFORE INSERT ON institucion --falta el update
 FOR EACH ROW
 EXECUTE PROCEDURE insert_coordenada();
 
 CREATE TRIGGER perfil_insert_coordenada_trigger
-BEFORE INSERT ON perfil
+BEFORE INSERT ON perfil --falta el update
 FOR EACH ROW
 EXECUTE PROCEDURE insert_coordenada();
 
 CREATE TRIGGER set_latitud_longitud_origen_trigger
-BEFORE INSERT ON preferencias
+BEFORE INSERT ON preferencias 
 FOR EACH ROW
 EXECUTE PROCEDURE set_latitud_longitud_origen();
