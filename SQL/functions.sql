bec32840bf21d2ecfd541fc2a3c31cc4c1d93991
Ana Shek <73360276+anna-ayn@users.noreply.github.com>
2024-06-11 22:48:13 -0400

    Update functions.sql

--- SQL/functions.sql
+++ SQL/functions.sql
@@ -9,13 +9,16 @@ $$
  LANGUAGE plpgsql;
 
 
-CREATE OR REPLACE FUNCTION insert_coordenada_origen()
+CREATE OR REPLACE FUNCTION set_latitud_longitud_origen()
 RETURNS TRIGGER AS
 $$
 BEGIN
-    IF New.longitud_origen IS NOT NULL AND New.latitud_origen IS NOT NULL THEN
-        New.coordenada_origen = ST_SetSRID(ST_MakePoint(New.longitud_origen, New.latitud_origen), 4326);
+    IF New.longitud_origen IS NULL OR New.latitud_origen IS NULL THEN
+        SELECT p.latitud, p.longitud INTO NEW.latitud_origen, NEW.longitud_origen
+        FROM perfil p
+        WHERE p.id_cuenta = NEW.id_cuenta;
     END IF;
+	New.coordenada_origen = ST_SetSRID(ST_MakePoint(New.longitud_origen, New.latitud_origen), 4326);
     RETURN NEW;
 END;
 $$
