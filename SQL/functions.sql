982d1a76552039611c42647491ed5177d237bafe
Ana Shek <73360276+anna-ayn@users.noreply.github.com>
2024-07-03 12:57:55 -0400

    Update functions.sql

--- SQL/functions.sql
+++ SQL/functions.sql
@@ -1,14 +1,32 @@
-CREATE OR REPLACE FUNCTION insert_coordenada()
+/*
+ * Funcion: set_coordenada()
+ *
+ * Uso: Cuando una fila se hace update o se inserta en una tabla que contenga las columnas de latitud y longitud se ejecuta automaticamente este trigger. Se setea la columna 'coordenada' creando un punto con los valores de 'longitud' y 'latitud'. El punto se asigna el SRID 4326.      
+ *
+ * Parametros: Ninguna
+
+ * Retorna: La funcion trigger retorna la nueva fila con la coordenada seteada
+ */
+CREATE OR REPLACE FUNCTION set_coordenada()
 RETURNS TRIGGER AS
 $$
 BEGIN
-	New.coordenada = ST_SetSRID(ST_MakePoint(New.longitud, New.latitud), 4326);
+    New.coordenada = ST_SetSRID(ST_MakePoint(New.longitud, New.latitud), 4326);
     RETURN NEW;
 END;
 $$
- LANGUAGE plpgsql;
+LANGUAGE plpgsql;
 
 
+/*
+ * funcion: set_latitud_longitud_origen()
+ *
+ * Uso: Cuando una fila se inserta o se hace update en la tabla 'preferencias' se ejecuta automaticamente este trigger. Se setea la columna 'coordenada_origen' creando un punto con los valores de 'longitud_origen' y 'latitud_origen'. El punto se asigna el SRID 4326. Si los valores de 'longitud_origen' y 'latitud_origen' son nulos (esto ocurre cuando se inserta una nueva fila), se setean con los valores de 'longitud' y 'latitud' de la tabla 'perfil' que tenga el mismo 'id_cuenta' que la fila insertada en 'preferencias'.
+ *
+ * Parametros: Ninguna
+ *
+ * Retorna: La funcion trigger retorna la nueva fila con la coordenada de origen insertada en la tabla 'preferencias'.
+ */
 CREATE OR REPLACE FUNCTION set_latitud_longitud_origen()
 RETURNS TRIGGER AS
 $$
