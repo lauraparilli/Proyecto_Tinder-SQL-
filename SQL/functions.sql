c0d691d8dc38572e1b816126baddcb805321ce04
Ana Shek <73360276+anna-ayn@users.noreply.github.com>
2024-07-04 13:18:18 -0400

    Update functions.sql

--- SQL/functions.sql
+++ SQL/functions.sql
@@ -43,21 +43,41 @@ $$
 
 LANGUAGE plpgsql;
 
+
+
 /*
-* Funcion: create_new_user
-*
-* Uso: crea un nuevo usuario en la base de datos, esto implica crear una cuenta, un perfil, relacionarlo con alguna institucion existente, e insertar en la bd una foto del usuario
-*
-* Parametros: nombre_u, apellido_u, fecha_nacimiento_u, telefono_u, email_u, password_hash, idioma_u, notificaciones_u, tema_u, sexo_u, latitud_u, longitud_u, foto_u, dominio_institucion, titulo_u, anio_ingreso, anio_egreso
-*
-* Retorna: Nada
-*/
-CREATE OR REPLACE FUNCTION create_new_user(nombre_u TEXT, apellido_u TEXT, fecha_nacimiento_u DATE, telefono_u TEXT, email_u TEXT, password_hash TEXT, idioma_u CHAR, notificaciones_u BOOLEAN, tema_u BOOLEAN, sexo_u TEXT, latitud_u DECIMAL(10, 8), longitud_u DECIMAL(11,8), foto_u TEXT, dominio_institucion TEXT, titulo_u TEXT, anio_ingreso INTEGER, anio_egreso INTEGER) 
+ * Función: create_new_user
+
+ * Uso: Crea un nuevo usuario en la base de datos con la información proporcionada.
+
+ * Parámetros:
+ *   - nombre_u: Texto que representa el nombre del usuario.
+ *   - apellido_u: Texto que representa el apellido del usuario.
+ *   - fecha_nacimiento_u: Fecha de nacimiento del usuario.
+ *   - telefono_u: Texto que representa el número de teléfono del usuario.
+ *   - email_u: Texto que representa el correo electrónico del usuario.
+ *   - password_hash: Texto que representa el hash de la contraseña del usuario.
+ *   - idioma_u: Carácter que representa el idioma preferido del usuario.
+ *   - notificaciones_u: Valor booleano que indica si el usuario desea recibir notificaciones.
+ *   - tema_u: Valor booleano que indica el tema preferido del usuario.
+ *   - sexo_u: Texto que representa el sexo del usuario.
+ *   - latitud_u: Valor decimal que representa la latitud de la ubicación del usuario.
+ *   - longitud_u: Valor decimal que representa la longitud de la ubicación del usuario.
+ *   - foto_u: Arreglo de textos que representa las fotos del usuario en formato base64.
+ *   - dominio_institucion: Texto que representa el dominio de la institución a la que estudio el usuario.
+ *   - titulo_u: Texto que representa el título académico del usuario.
+ *   - anio_ingreso: Valor entero que representa el año de ingreso a la institución.
+ *   - anio_egreso: Valor entero que representa el año de egreso de la institución.
+ *
+ * Retorna: Nada.
+ */
+CREATE OR REPLACE FUNCTION create_new_user(nombre_u TEXT, apellido_u TEXT, fecha_nacimiento_u DATE, telefono_u TEXT, email_u TEXT, password_hash TEXT, idioma_u CHAR, notificaciones_u BOOLEAN, tema_u BOOLEAN, sexo_u TEXT, latitud_u DECIMAL(10, 8), longitud_u DECIMAL(11,8), foto_u TEXT[], dominio_institucion TEXT, titulo_u TEXT, anio_ingreso INTEGER, anio_egreso INTEGER) 
 RETURNS VOID 
 LANGUAGE plpgsql
 AS $$
 DECLARE
     id_cuenta_u INTEGER;
+    i INTEGER;
 BEGIN
     /* verificar que el correo o telefono no exista */
     IF EXISTS (SELECT * FROM cuenta WHERE email = email_u OR telefono = telefono_u) THEN
@@ -75,7 +95,8 @@ BEGIN
 
     INSERT INTO estudio_en(id_cuenta, dominio, titulo, ano_ingreso, ano_egreso) VALUES (id_cuenta_u, dominio_institucion, titulo_u, anio_ingreso, anio_egreso);
 
-    INSERT INTO tiene_foto (id_cuenta, foto) VALUES (id_cuenta_u, decode(foto_u, 'base64'));
-
+    FOR i IN 1..array_length(foto_u, 1) LOOP
+        INSERT INTO tiene_foto (id_cuenta, foto) VALUES (id_cuenta_u, decode(foto_u[i], 'base64'));
+    END LOOP;
 END;
 $$;
