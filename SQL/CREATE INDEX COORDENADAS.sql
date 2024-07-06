da7ef2c5c94c9debdc3bd001e9cc838cef122572
Ana Shek <73360276+anna-ayn@users.noreply.github.com>
2024-07-06 09:46:39 -0400

    Update CREATE INDEX COORDENADAS.sql

--- SQL/CREATE INDEX COORDENADAS.sql
+++ SQL/CREATE INDEX COORDENADAS.sql
@@ -1,4 +1,5 @@
-CREATE INDEX perfil_geo_index ON perfil USING GIST (coordenada);
+CREATE INDEX perfil_geo_index ON perfil USING GIST (coordenada);  -- para buscar personas por max distancia
 
-CREATE INDEX pref_geo_index ON preferencias USING GIST (coordenada_origen);
+CREATE INDEX pref_geo_index ON preferencias USING GIST (coordenada_origen); -- para buscar personas por coordenada origen
 
+CREATE INDEX ON cuenta (fecha_nacimiento); -- para buscar personas por max o min edad
