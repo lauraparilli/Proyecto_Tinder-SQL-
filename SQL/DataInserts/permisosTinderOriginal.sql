f9d72ded4851344bf490a6e1b1fc07ec696f50b0
Ana Shek <73360276+anna-ayn@users.noreply.github.com>
2024-07-07 16:59:01 -0400

    Update permisosTinderOriginal.sql

--- SQL/DataInserts/permisosTinderOriginal.sql
+++ SQL/DataInserts/permisosTinderOriginal.sql
@@ -12,6 +12,7 @@ SELECT insert_new_permission('semSuperlikes', 'Super Likes semanales', 'Gold');
 SELECT insert_new_permission('1freeBoost', '1 Boost gratis al mes', 'Gold');
 SELECT insert_new_permission('whoLikesU', 'Descubre a quien le gustas', 'Gold');
 SELECT insert_new_permission('newTopPicks', 'Nuevos Top Picks todos los dias', 'Gold');
+SELECT insert_old_tier_old_permissions('Gold', ARRAY['infLikes', 'infRewinds', 'passport', 'adBlock', 'incognite']);
 
 SELECT 
 
@@ -19,3 +20,4 @@ SELECT
 SELECT insert_new_permission('sendMsgEveryone', 'Envia un mensaje antes de hacer match', 'Platinum');
 SELECT insert_new_permission('prefLikes', 'Likes con preferencia', 'Platinum');
 SELECT insert_new_permission('last7DaysHistoryLikes', 'Acceso a los Likes que enviaste en los ultimos 7 dias', 'Platinum');
+SELECT insert_old_tier_old_permissions('Platinum', ARRAY['infLikes', 'infRewinds', 'passport', 'adBlock', 'incognite', 'semSuperlikes', 'whoLikesU', 'newTopPicks']);
